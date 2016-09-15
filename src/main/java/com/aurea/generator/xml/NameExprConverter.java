3d9b90457812154711507e88968b542320da0f24
Otanikotani <otanikotani@gmail.com>
2016-09-15 11:36:18 +0300

    Add files via upload

---
+++ src/main/java/com/aurea/generator/xml/NameExprConverter.java
@@ -0,0 +1,21 @@
+package com.aurea.generator.xml;
+
+import com.github.javaparser.ast.expr.NameExpr;
+import com.thoughtworks.xstream.converters.SingleValueConverter;
+
+public class NameExprConverter implements SingleValueConverter {
+    @Override
+    public String toString(Object obj) {
+        return ((NameExpr)obj).getName();
+    }
+
+    @Override
+    public Object fromString(String str) {
+        return new NameExpr(str);
+    }
+
+    @Override
+    public boolean canConvert(Class type) {
+        return NameExpr.class.equals(type);
+    }
+}
